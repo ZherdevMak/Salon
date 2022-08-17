421b015b063372453abf1d9596faad8c1851ca1f
ZherdevMak <Zherdev.mak@gmail.com>
2022-08-17 23:53:07 +0300

    nav menu 2 step

--- src/Header/Nav/Nav.tsx
+++ src/Header/Nav/Nav.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import stl from './Nav.module.scss'
+import menu from '../../Assets/images/menu-icon.png'
 
 const Nav = () => {
     return (
-        <div className={stl.wrapper} >
-            <h1 className={stl.container}>
-                <h1 className={stl.text}>
-                    SALON SAFARI
-                    </h1>
-            </h1>
+        <div className={stl.wrapper}>
+            <div className={stl.container}>
+                <div className={stl.textContainer}>
+                    <h1 className={stl.text}>SALON SAFARI</h1>
+                </div>
+                <div className={stl.iconWrapper}>
+                        <img src={menu} alt=""/>
+                </div>
+            </div>
 
         </div>
     );
