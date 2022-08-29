70c643cc0a9fe2e7d089f167f22bdd74706c76e5
ZherdevMak <Zherdev.mak@gmail.com>
2022-08-29 12:24:17 +0300

    AppointmentSection done

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ function App() {
             <Exellence/>
             <Services/>
             <Appointment/>
+            <Appointment/>
 
         </div>
     );
--- src/Main/Appointment/Appointment.tsx
+++ src/Main/Appointment/Appointment.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import stl from './Appointment.module.scss'
-import photo1 from "../../Assets/images/photo-1.jpg"
-import photo2 from "../../Assets/images/photo-2.jpg"
-import photo3 from "../../Assets/images/photo-3.jpg"
 
 const Appointment = () => {
     return (
@@ -10,13 +7,9 @@ const Appointment = () => {
             <div className={stl.container}>
                 <div className={stl.TextContainer}>
                     <h2 className={stl.Text}>make an appointment</h2>
-                    <p>Call us now to book an appointment with one of our hair stylist!</p>
+                    <p className={stl.descriptionText}>Call us now to book an appointment with one of our hair stylist!</p>
                 </div>
-                <div className={stl.imageContainer}>
-                    <img className={stl.image} src={photo1} alt="salon interior"/>
-                    <img className={stl.image} src={photo2} alt="salon hall"/>
-                    <img className={stl.image} src={photo3} alt="towels"/>
-                </div>
+                <a className={stl.callButton} href="tel:+79689471982">Call Now</a>
             </div>
         </section>
     );
