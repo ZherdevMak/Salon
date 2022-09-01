ee534bbb5a089acb75e970c1f96ec99dc5e89d3f
ZherdevMak <Zherdev.mak@gmail.com>
2022-09-01 21:12:16 +0300

    sliderText Media Query

--- src/Main/Slider/Slider.tsx
+++ src/Main/Slider/Slider.tsx
@@ -17,7 +17,7 @@ const Slider = () => {
                 <Zoom scale={0.4} indicators={false} arrows={false} duration={4000}>
                     {images.map((each, index) => (
                         <div key={index} style={{width: "100%"}}>
-                            <img style={{objectFit: "cover", width: "100%", height: '47vh'}} alt="Slide Image"
+                            <img style={{objectFit: "cover", width: "100%", height: '100%'}} alt="Two girls"
                                  src={each}/>
                         </div>
                     ))}
