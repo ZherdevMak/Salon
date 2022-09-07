e026013789d1f91e0f217b514aa40e2156699a68
ZherdevMak <Zherdev.mak@gmail.com>
2022-09-07 23:31:21 +0300

    edit capital menu start numbers

--- src/Header/Nav/MobileMenu/MobileMenu.tsx
+++ src/Header/Nav/MobileMenu/MobileMenu.tsx
@@ -4,13 +4,13 @@ const MobileMenu = () => {
     return (
         <div>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+                <li>7</li>
             </ul>
         </div>
     );
