b57b3473fd6d58600718788088e2eb595a624be3
ZherdevMak <Zherdev.mak@gmail.com>
2022-09-08 20:45:53 +0300

    edit capital menu new style

--- src/Header/Nav/MobileMenu/MobileMenu.tsx
+++ src/Header/Nav/MobileMenu/MobileMenu.tsx
@@ -11,6 +11,7 @@ const MobileMenu = () => {
                 <li>5</li>
                 <li>6</li>
                 <li>7</li>
+                <div></div>
             </ul>
         </div>
     );
